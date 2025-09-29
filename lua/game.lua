96c71d91b088b3ce56fc190b86bc58601b2dbbb7
user8595 <muhikhsansyakir@gmail.com>
2025-09-29 15:52:23 +0800

    increase combo cooldown time

--- lua/game.lua
+++ lua/game.lua
@@ -141,7 +141,7 @@ function comboCooldown(dt)
     if stats.combo > 0 then
         comboTime = comboTime + dt
     end
-    if comboTime > 2 then
+    if comboTime > 5 then
         stats.combo = 0
         comboTime = 0
     end
